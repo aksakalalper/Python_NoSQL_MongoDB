ace59cd4a240981f03e0d1efd8957bc7464f2e12
aksakalalper <aksakalalper@gmail.com>
2024-10-23 19:40:43 +0300

    Update ders1MongoDBVeritabaniBaglanti.py

--- ders1MongoDBVeritabaniBaglanti.py
+++ ders1MongoDBVeritabaniBaglanti.py
@@ -1,14 +1,14 @@
-# kutuphane import edildi.
 import pymongo
-# alici bilgisi girilir.
-client=pymongo.MongoClient("mongodb://localhost:27017")
-db=client["node-app"]
-collection=db["products"]
-# Tüm belgeleri al
-veriler = collection.find()
 
-# Verileri yazdır
-for veri in veriler:
-    print(veri)
-
-print(list(client.list_databases()))
+uri = "mongodb+srv://aksakal4646:373737@cluster0.ohlhc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+# Create a new client and connect to the server
+client = pymongo.MongoClient(uri)
+# Send a ping to confirm a successful connection
+try:
+    # Tüm veritabanı isimlerini alma
+    databases = client.list_database_names()
+# Veritabanı isimlerini yazdırma
+    for db_name in databases:
+        print(db_name)
+except Exception as e:
+    print(e)
