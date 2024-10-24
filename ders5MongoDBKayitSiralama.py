cc87c52631d90840f5e9a69dae89fed24c4feee8
aksakalalper <aksakalalper@gmail.com>
2024-10-24 23:42:47 +0300

    Update ders5MongoDBKayitSiralama.py

--- ders5MongoDBKayitSiralama.py
+++ ders5MongoDBKayitSiralama.py
@@ -1,7 +1,7 @@
 import pymongo
 from bson.objectid import ObjectId
 
-uri = "mongodb+srv://aksakal4646:373737@cluster0.ohlhc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://aksakal4646:******@cluster0.ohlhc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
 client = pymongo.MongoClient(uri)
 try:
